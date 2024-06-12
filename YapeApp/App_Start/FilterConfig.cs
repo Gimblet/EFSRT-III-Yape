8ac19168e3ce659cc645075f9a55f4a823366445
Diego <gimbletdev@gmail.com>
2024-06-11 23:56:07 -0500

    Add project files.

---
+++ YapeApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace YapeApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
